fd8063439abe10bf6454e6a2e2176de5b2e29d10
ISMAEL <isma98.igm@gmail.com>
2019-11-28 19:51:22 +0100

    organizar carpetas test, e implementación base para poder empezar con los test

---
+++ scripts_limpieza/test/test_limpiarDatos.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+import limpiarDatos
+
+
+class TestLimpiarDatos(unittest.TestCase):
+    
+    def test_extraer_peliculas(self):
+        self.assertEqual(mymodule.extraer_peliculas(, ), )
+
+if __name__ == "__main__":
+    unittest.main()
