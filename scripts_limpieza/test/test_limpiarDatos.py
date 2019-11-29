f0dc26276dac5d8e96037b70dcecab997a7c6f76
ISMAEL <isma98.igm@gmail.com>
2019-11-29 11:27:16 +0100

    funcionando test de extraer peliculas, en principio pasa los dos

--- scripts_limpieza/test/test_limpiarDatos.py
+++ scripts_limpieza/test/test_limpiarDatos.py
@@ -5,9 +5,25 @@ import limpiarDatos
 
 
 class TestLimpiarDatos(unittest.TestCase):
-    
-    def test_extraer_peliculas(self):
-        self.assertEqual(limpiarDatos.extraer_peliculas(1, 1), '1,00:01:18 ,2007' + '\n')
 
+
+
+    
+    def test_extraer_peliculas_1(self):
+        with open("./movies.txt", 'r') as reader:  
+            lines = [line.strip() for line in reader]
+        pelicula = lines[0]
+        id_pelicula = 1
+        self.assertEqual(limpiarDatos.extraer_peliculas(pelicula, id_pelicula),'1,00:01:18 ,2007' +'\n')
+
+    def test_extraer_peliculas_2(self):
+        with open("./movies.txt", 'r') as reader:  
+            lines = [line.strip() for line in reader]
+        pelicula = lines[1]
+        id_pelicula = 2
+        self.assertEqual(limpiarDatos.extraer_peliculas(pelicula, id_pelicula),'2,002 agenti segretissimi ,1964' +'\n')
+  
+                    
+                 
 if __name__ == "__main__":
     unittest.main()
