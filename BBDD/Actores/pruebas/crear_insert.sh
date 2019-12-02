a80a66f45df56144ae92373d86f678d1682661b4
ISMAEL <isma98.igm@gmail.com>
2019-12-03 00:13:43 +0100

    modificando crear_insert ya que no esta incluido atributo id_actor

--- BBDD/Actores/pruebas/crear_insert.sh
+++ BBDD/Actores/pruebas/crear_insert.sh
@@ -2,4 +2,4 @@
 
 sed 's/ //g' actores.txt > actores_bueno.txt
 
-awk -F"," '{print "INSERT INTO Actores(apellido,nombre,id_actor,pais_de_origen, fecha_nacimiento,fecha_muerte VALUES(""\"" $1 "\","  "\"" $2 "\"," $3 "," """\"" "\"," "\"" "\"," "\"" "\""";"}' actores_bueno.txt > insert.txt
+awk -F"," 'BEGIN{count=1} {print "INSERT INTO Actores(apellido,nombre,id_pelicula,id_actor,pais_de_origen, fecha_nacimiento,fecha_muerte) VALUES(""\"" $1 "\","  "\"" $2 "\"," $3  "," count++ "," """\"undefined" "\"," "\"undefined" "\"," "\"undefined" "\""");"}' actores_bueno.txt > insert.txt
