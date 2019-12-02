2f678dfc0fe1c6b397c23efb0d5e5616e5b39358
Angela <ange.isabel@hotmail.com>
2019-12-02 13:26:35 +0100

    Subo queries y datos de generos

--- queries.sql
+++ queries.sql
@@ -1,8 +1,16 @@
+
 SELECT
-p.nombre, c.nombre
-from peliculas as p
+m.nombre, c.nombre
+from peliculas as m
 join adecuado_para as a
-on a.id_pelicula = p.id_pelicula
+on a.id_pelicula = m.id_pelicula
 join calificacion as c 
 on a.id_calificacion = c.id_calificacion
 
+SELECT
+m.nombre, g.nombre
+from peliculas as m
+join pertenece as p
+on m.id_pelicula = p.id_pelicula
+join generos as g
+on p.id_genero = g.id_genero
