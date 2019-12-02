693ebb37b04217dce9f98df9d65725b12b8a609a
ISMAEL <isma98.igm@gmail.com>
2019-12-02 23:15:33 +0100

    arreglando crear_insert ya que los atributos son VARCHAR y es necesario añadir comillas dobles, si no no funciona

--- BBDD/Actores/pruebas/crear_insert.sh
+++ BBDD/Actores/pruebas/crear_insert.sh
@@ -2,4 +2,4 @@
 
 sed 's/ //g' actores.txt > actores_bueno.txt
 
-awk -F"," '{print "INSERT INTO Actores(apellido,nombre,id_actor,pais_de_origen, fecha_nacimiento,fecha_muerte VALUES(""\"" $1 "\","  "\"" $2 "\"," $3 ", undefined," "undefined," "undefined);"}' actores_bueno.txt > insert.txt
+awk -F"," '{print "INSERT INTO Actores(apellido,nombre,id_actor,pais_de_origen, fecha_nacimiento,fecha_muerte VALUES(""\"" $1 "\","  "\"" $2 "\"," $3 "," """\"" "\"," "\"" "\"," "\"" "\""";"}' actores_bueno.txt > insert.txt
