41f026e5aaffd279087684f2615c7701b068fe47
ISMAEL <isma98.igm@gmail.com>
2019-12-03 13:11:15 +0100

    modificando crear_insert y readme_cmd para arreglar problema de los actores sin apellido, a falta de mas pruebas

--- BBDD/Actores/pruebas/crear_insert.sh
+++ BBDD/Actores/pruebas/crear_insert.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sed 's/ //g' actores.txt > actores_bueno.txt
+#sed 's/ //g' actores.txt > actores_bueno.txt
 
-awk -F"," 'BEGIN{count=1} {print "INSERT INTO Actores(apellido,nombre,id_pelicula,id_actor,pais_de_origen, fecha_nacimiento,fecha_muerte) VALUES(""\"" $1 "\","  "\"" $2 "\"," $3  "," count++ "," """\"undefined" "\"," "\"undefined" "\"," "\"undefined" "\""");"}' actores_bueno.txt > insert.txt
+awk -F"," 'BEGIN{count=1}
+	{if($3 != NULL)print "INSERT INTO Actores(apellido,nombre,id_pelicula,id_actor,pais_de_origen, fecha_nacimiento,fecha_muerte) VALUES(""\"" $1 "\","  "\"" $2 "\"," $3  "," count++ "," """\"undefined" "\"," "\"undefined" "\"," "\"undefined" "\""");"; else if($3==NULL) print "INSERT INTO Actores(apellido,nombre,id_pelicula,id_actor,pais_de_origen, fecha_nacimiento,fecha_muerte) VALUES(""""\"undefined" "\","  "\"" $1 "\"," $2  "," count++ "," """\"undefined" "\"," "\"undefined" "\"," "\"undefined" "\""");"}' actores.txt > insert.txt
