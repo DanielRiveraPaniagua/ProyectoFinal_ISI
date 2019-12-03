bd1dc2fb0ea998850daef8092d9b43296f0b8b36
ISMAEL <isma98.igm@gmail.com>
2019-12-03 19:10:29 +0100

    modificando script de peliculas, los insert, no completaba todos los datos

--- BBDD/Peliculas/script_insert_peliculas.sh
+++ BBDD/Peliculas/script_insert_peliculas.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-sed 's/ //g' peliculas.txt > peliculas_prueba_2.txt
-
-awk -F"," '{print "INSERT INTO Peliculas(id_pelicula,nombre,anyo,duracion,rating) VALUES(" $1 ","  "\"" $2 "\"," $3  "," """\"undefined" "\"," undefined" "");"}' peliculas_prueba_2.txt > peliculas_prueba_3.txt
+awk -F"," '{print "INSERT INTO Peliculas(id_pelicula,nombre,anyo,duracion,rating) VALUES(" $1 ","  "\"" $2 "\"," $3  "," """\"undefined" "\","  "undefined" ");"}' peliculas.txt > insert.txt
 
