278928916da8a26faedd301b7d00bc7244ae66f5
ISMAEL <isma98.igm@gmail.com>
2019-12-04 13:30:56 +0100

    añadiendo a datos limpios el fichero de datos que se va usar y eliminado ficheros inecesarios

--- BBDD/Peliculas/script_insert_peliculas.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#sed 's/ //g' peliculas.txt > peliculas_1.txt
-
-#sed s/"'"/" "/g peliculas_1.txt > peliculas_2.txt
-
-awk -F"|" '{print "INSERT INTO Peliculas(id_pelicula,nombre,anyo,duracion,rating) VALUES(" $1 ","  "\"" $2 "\"," $3  "," """\"undefined" "\","  "0" ");"}' peliculas_2.txt > insert.txt
-
