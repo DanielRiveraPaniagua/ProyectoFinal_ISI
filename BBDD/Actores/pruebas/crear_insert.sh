3f6e1d671f6bde8b88b5e28c07d8de0d0decc2df
ISMAEL <isma98.igm@gmail.com>
2019-12-04 12:18:20 +0100

    subiendo nuevo file bash para crear fichero de texto de actores y subirlo al servidor

--- BBDD/Actores/pruebas/crear_insert.sh
+++ BBDD/Actores/pruebas/crear_insert.sh
@@ -3,4 +3,4 @@
 #sed 's/ //g' actores.txt > actores_bueno.txt
 
 awk -F"," 'BEGIN{count=1}
-	{if($3 != NULL)print "INSERT INTO Actores(apellido,nombre,id_pelicula,id_actor,pais_de_origen, fecha_nacimiento,fecha_muerte) VALUES(""\"" $1 "\","  "\"" $2 "\"," $3  "," count++ "," """\"undefined" "\"," "\"undefined" "\"," "\"undefined" "\""");"; else if($3==NULL) print "INSERT INTO Actores(apellido,nombre,id_pelicula,id_actor,pais_de_origen, fecha_nacimiento,fecha_muerte) VALUES(""""\"undefined" "\","  "\"" $1 "\"," $2  "," count++ "," """\"undefined" "\"," "\"undefined" "\"," "\"undefined" "\""");"}' actores.txt > insert.txt
+	{if($3 != NULL)print "\"" $1 "\","  "\"" $2 "\"," $3  "," count++ "," """\"undefined" "\"," "\"undefined" "\"," "\"undefined" "\""; else if($3==NULL) print """\"undefined" "\","  "\"" $1 "\"," $2  "," count++ "," """\"undefined" "\"," "\"undefined" "\"," "\"undefined" "\""}' actores.txt > datos_actores.txt
