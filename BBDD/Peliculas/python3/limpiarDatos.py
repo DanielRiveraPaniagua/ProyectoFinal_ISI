29e8bb8d43e06c95cd8aeda194f59d5f32060074
ISMAEL <isma98.igm@gmail.com>
2019-12-04 11:41:22 +0100

    modificado fichero python para separar peliculas con pipe, ya que existen peliculas con coma

---
+++ BBDD/Peliculas/python3/limpiarDatos.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3.6
+peliculas = open("./peliculas.txt", "w")
+actores = open("./actores.txt", "w")
+
+"""
+Esta funciona extrae las peliculas con su fecha y agrega un indice unico a cada
+peliculas
+"""
+def extraer_peliculas(line, id_pelicula):
+	pelicula = line.split("/")[0].split("(")
+	ano = pelicula[1].replace(')','') 
+	if len(ano) != 4: 
+		return str(id_pelicula) + '|' + pelicula[0] + '|' + '0000'.replace(')','') + '\n' 
+	else: 
+		return str(id_pelicula) + '|' + pelicula[0] + '|' + pelicula[1].replace(')','') + '\n'
+
+"""
+Esta funcion extrae los nombres y apellidos de los actores que han trabajado en
+cada pelicula
+"""
+def extraer_actores(line, id_pelicula):
+    salida = ''
+    elementos = line.split("/")[1:]
+    for actor in elementos:
+        salida += actor.replace("\n","") + ',' + str(id_pelicula) + '\n'
+    return salida
+
+id_pelicula = 1
+with open("./movies.txt", 'r') as reader:
+    for line in reader:
+        # print(extraer_peliculas(line, id_pelicula))
+        # print(extraer_actores(line, id_pelicula))
+        peliculas.write(extraer_peliculas(line, id_pelicula))
+        actores.write(extraer_actores(line, id_pelicula))
+        id_pelicula += 1
+        actores
+
+
+
+peliculas.close()
+actores.close()
--- BBDD/Peliculas/script_insert_peliculas.sh
+++ BBDD/Peliculas/script_insert_peliculas.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-awk -F"," '{print "INSERT INTO Peliculas(id_pelicula,nombre,anyo,duracion,rating) VALUES(" $1 ","  "\"" $2 "\"," $3  "," """\"undefined" "\","  "undefined" ");"}' peliculas.txt > insert.txt
+#sed 's/ //g' peliculas.txt > peliculas_1.txt
+
+#sed s/"'"/" "/g peliculas_1.txt > peliculas_2.txt
+
+awk -F"|" '{print "INSERT INTO Peliculas(id_pelicula,nombre,anyo,duracion,rating) VALUES(" $1 ","  "\"" $2 "\"," $3  "," """\"undefined" "\","  "0" ");"}' peliculas_2.txt > insert.txt
 
