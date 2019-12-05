1ca5ac981c9f75d28b87f6bd493d7ee7cf84c7fb
felipecordente <felipe_1998_@outlook.com>
2019-12-05 18:01:48 +0100

    modificado carpeta Directores_Guionistas_Personas_Generos

--- Directores_Guionistas_Personas_Generos/process_data_genres.py
+++ Directores_Guionistas_Personas_Generos/process_data_genres.py
@@ -5,20 +5,18 @@ generos = open("./generos.txt", "w")
 
 def extraer_generos(line, id_pelicula):
     cadena = ""
-    print(id_pelicula)
-    if id_pelicula == 1102256:
-        print("entra")        
-        print(line[0:])
+
     try:
         gs = line[8].split(',')
     except IndexError:
+        #print(line[0:])
         return cadena
-    
-    #print(line[8].split(','))
+
     for g in gs:
         cadena = cadena + str(id_pelicula) + ',' + line[0] + ',' + g + '\n'
         #print("->" + cadena)
     return cadena
+
 
 with open("./title.basics.tsv", 'r') as reader:
     n = 0
@@ -29,9 +27,11 @@ with open("./title.basics.tsv", 'r') as reader:
         if id_pelicula != 0:
             generos.write(extraer_generos(line, id_pelicula))
         n=n+1
-        #limitador de datos
+        #limitador de datos de salidad
+        """
         if n == 10000:
            break
+        """
         id_pelicula = id_pelicula + 1
 
 generos.close()
