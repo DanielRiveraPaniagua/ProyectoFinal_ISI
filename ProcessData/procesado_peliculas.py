a2db599b0a9478607ffcfbfe97985b18f0f38ca1
felipecordente <felipe_1998_@outlook.com>
2019-12-06 15:43:23 +0100

    modificado ProcessData/procesado_peliculas.py

--- ProcessData/procesado_peliculas.py
+++ ProcessData/procesado_peliculas.py
@@ -6,25 +6,21 @@ movies = open("./movies.txt", "w")
 
 r = csv.reader(peliculas, delimiter = ",")
 
+
+for row in r:
+n = 0
+title = row[1]
+print(title)
 with open('title.basics.tsv','r') as tsv:
     re = csv.reader(tsv, delimiter = "\t")
-    for row in r:
-        n = 0
-        title = row[1]
-        print(title)
-        for fila in re:
-            # No encuentra la pelicula y solo da una vuelta dentro
-            #n += 1
-            #if n == 1:
-                #print(fila)
-            if title == fila[3]:
-                #print("entramos")
-                id = fila[0]
-                fecha = row[2]
-                movies.write(str(id) + ',' + title + ',' + fecha + '\n')
-                break
-
-
+    for fila in re:
+        print("->" + fila[3])
+        if title == fila[3]:
+            #print("entramos")
+            id = fila[0]
+            fecha = row[2]
+            movies.write(str(id) + ',' + title + ',' + fecha + '\n')
+            break
 
 peliculas.close()
 movies.close()
