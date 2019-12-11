5f1bfb6300f9101cb5b9cc5bb28afa906d4e678b
felipecordente <felipe_1998_@outlook.com>
2019-12-11 16:36:19 +0100

    vaciado de repo

--- BBDD/Actores/java/crear_insert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#sed 's/ //g' actores.txt > actores_bueno.txt
-
-awk -F"," 'BEGIN{count=1}
-	{if($3 != NULL)print "\"" $1 "\","  "\"" $2 "\"," $3  "," count++ "," """\"undefined" "\"," "\"undefined" "\"," "\"undefined" "\""; else if($3==NULL) print """\"undefined" "\","  "\"" $1 "\"," $2  "," count++ "," """\"undefined" "\"," "\"undefined" "\"," "\"undefined" "\""}' actores.txt > datos_actores.txt
