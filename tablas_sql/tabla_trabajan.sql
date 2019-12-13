71ffe3d4f98961f064edf23cec4b9ab4d4270005
oscarescuderoarnanz <escuderoarnanzoscar@gmail.com>
2019-12-13 21:38:46 +0100

    Borro archivos innecesario

--- tablas_sql/tabla_trabajan.sql
+++
@@ -1,7 +0,0 @@
-.open sample.db
-
-CREATE TABLE trabajan(id_pelicula INT NOT NULL,
-		      id_actor INT NOT NULL,
-		      PRIMARY KEY(id_pelicula, id_actor)
-		      FOREIGN KEY id_pelicula REFERENCES peliculas(id_pelicula)
-		      FOREIGN KEY id_actor REFERENCES actores(id_actor));
