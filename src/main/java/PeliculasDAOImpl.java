c6097b16bcd332990028ca52cc9827f2ddbfd346
JavierBravoDonaire <jjbd98@hotmail.com>
2019-12-13 18:39:21 +0100

    añado ActoresDAOImpl.java y arreglo algunos fallos PeliculasDAOImpl.java

--- src/main/java/PeliculasDAOImpl.java
+++ src/main/java/PeliculasDAOImpl.java
@@ -9,17 +9,17 @@ import java.util.StringTokenizer;
 import javax.servlet.MultipartConfigElement;
 import java.io.*;
 
-public class PeliculasDAOImpl extends GenericDAOImpl {
+public class PeliculasDAOImpl extends GenericDAOImpl implements InterfacePeliculasDAO{
 
 	public PeliculasDAO getPeliculabyID(int id){
-		String query = "SELECT * FROM " + peliculas + " WHERE id_pelicula=" + id;
+		String query = "SELECT * FROM peliculas WHERE id_pelicula=" + id;
 		GenericDAOImpl generic = new GenericDAOImpl();
 		Connection conn = generic.connect();
 		try(PreparedStatement pstmt = conn.prepareStatement(query)){
 			ResultSet rs = pstmt.executeQuery();
 			connection.commit(); // ESTE CONECTION NO SE SI FUNCIONARIA
 			while(rs.next()){
-				PeliculasDAO Pelicula = new PeliculasDAO;
+				PeliculasDAO Pelicula = new PeliculasDAO();
 
 				Pelicula.setId(rs.getInt("id_pelicula"));
 				Pelicula.setNombre(rs.getString("nombre"));
