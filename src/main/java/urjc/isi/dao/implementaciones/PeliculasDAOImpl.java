f876136994fdff0a4acebaecca5b9ac50074fa32
Noozh <sirl.daniel.arevalo@hotmail.com>
2019-12-27 10:49:19 +0100

    Arreglados mas problemas de indexacion

--- src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
+++ src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 //servidor
 public class PeliculasDAOImpl extends GenericDAOImpl<Peliculas> implements PeliculasDAO{
 
-  public Peliculas fromResultSet(ResultSet rs) throws  SQLException{
+	public Peliculas fromResultSet(ResultSet rs) throws  SQLException{
 		Peliculas peli = new Peliculas();
 
 		peli.setIdPelicula(rs.getString("idpelicula"));
@@ -30,86 +30,89 @@ public class PeliculasDAOImpl extends GenericDAOImpl<Peliculas> implements Pelic
 		return peli;
 	}
   
-  @Override
-  public void createTable() throws SQLException{
+	@Override
+	public void createTable() throws SQLException{
 		Statement statement = c.createStatement();
 		statement.executeUpdate("create table peliculas (idpelicula text, titulo text, año text, duracion text, rating Decimal(4,2), nvotos INT, PRIMARY KEY (idpelicula))");
 		c.commit();	
 	}
   
-  @Override
-  public void dropTable() throws SQLException {
+	@Override
+	public void dropTable() throws SQLException {
 		Statement statement = c.createStatement();
 		statement.executeUpdate("drop table if exists peliculas");
 		c.commit();
 	}
   
-  @Override
-  public void insert(Peliculas entity) {
-  	String sql = "INSERT INTO peliculas(idpelicula,titulo,año,duracion,rating,nvotos) VALUES(?,?,?,?,?,?)";
-
-  	try (PreparedStatement pstmt = c.prepareStatement(sql)) {
-  		pstmt.setString(1, entity.getIdPelicula());
-  		pstmt.setString(2, entity.getTitulo());
-  		pstmt.setInt(3, entity.getAño());
-      	pstmt.setDouble(4, entity.getDuracion());
-      	pstmt.setDouble(5, entity.getRating());
-      	pstmt.setInt(6, entity.getNVotos());
-  		pstmt.executeUpdate();
-    } catch (SQLException e) {
-  	    System.out.println(e.getMessage());
-  	}
-  }
+	@Override
+	public void insert(Peliculas entity) {
+	  	String sql = "INSERT INTO peliculas(idpelicula,titulo,año,duracion,rating,nvotos) VALUES(?,?,?,?,?,?)";
+	
+	  	try (PreparedStatement pstmt = c.prepareStatement(sql)) {
+	  		pstmt.setString(1, entity.getIdPelicula());
+	  		pstmt.setString(2, entity.getTitulo());
+	  		pstmt.setInt(3, entity.getAño());
+	      	pstmt.setDouble(4, entity.getDuracion());
+	      	pstmt.setDouble(5, entity.getRating());
+	      	pstmt.setInt(6, entity.getNVotos());
+	  		pstmt.executeUpdate();
+	    } catch (SQLException e) {
+	  	    System.out.println(e.getMessage());
+	  	}
+	}
   
-  @Override
-  public void uploadTable(BufferedReader br) throws IOException, SQLException {
-    String s;
-    while ((s = br.readLine()) != null) {
-      Peliculas pelicula = new Peliculas(s);
-      insert(pelicula);
-      c.commit();
-    }
-  }
-  
-  @Override
-  public List<Peliculas> selectAll(){
-	  List<Peliculas> filmList = new ArrayList<>();
-	  String sql = "SELECT * from peliculas";
-	  try (PreparedStatement pstmt = c.prepareStatement(sql)) {
-		  ResultSet rs = pstmt.executeQuery();
-		  c.commit();
-		  while(rs.next()){
-			  filmList.add(fromResultSet(rs));
-		  }
-    } catch (SQLException e) {
-		  System.out.println(e.getMessage());
-	  }
-	  return filmList;
-  }
-  @Override
-  public Peliculas selectByID (String idpelicula){
-	  String sql = "SELECT * from peliculas WHERE idpelicula=" + idpelicula;
-	  Peliculas peli = new Peliculas();
-	  try (PreparedStatement pstmt = c.prepareStatement(sql)) {
-		  ResultSet rs = pstmt.executeQuery();
-		  c.commit();
-		  peli = fromResultSet(rs);
-      } catch (SQLException e) {
-		  System.out.println(e.getMessage());
-	  }
-	  return peli;
-  }
-  @Override
-  public void deleteByID(String idpelicula){
-	  String sql = "DELETE from peliculas WHERE idpelicula=" + idpelicula;
-	  try (PreparedStatement pstmt = c.prepareStatement(sql)){
-		  pstmt.executeUpdate();
+	@Override
+	public void uploadTable(BufferedReader br) throws IOException, SQLException {
+		String s;
+	    while ((s = br.readLine()) != null) {
+	      Peliculas pelicula = new Peliculas(s);
+	      insert(pelicula);
 	      c.commit();
-	  } catch (SQLException e) {
-		  System.out.println(e.getMessage());
-	  }
-  }
-  @Override
+	    }
+	}
+  
+	@Override
+	public List<Peliculas> selectAll(){
+		List<Peliculas> filmList = new ArrayList<>();
+		String sql = "SELECT * from peliculas";
+		try (PreparedStatement pstmt = c.prepareStatement(sql)) {
+			ResultSet rs = pstmt.executeQuery();
+			c.commit();
+			while(rs.next()){
+				filmList.add(fromResultSet(rs));
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return filmList;
+	}
+	
+	@Override
+	public Peliculas selectByID (String idpelicula){
+		String sql = "SELECT * from peliculas WHERE idpelicula=" + idpelicula;
+		Peliculas peli = new Peliculas();
+		try (PreparedStatement pstmt = c.prepareStatement(sql)) {
+			ResultSet rs = pstmt.executeQuery();
+			c.commit();
+			peli = fromResultSet(rs);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return peli;
+	}
+  
+	@Override
+	public void deleteByID(String idpelicula){
+		String sql = "DELETE from peliculas WHERE idpelicula=" + idpelicula;
+		try (PreparedStatement pstmt = c.prepareStatement(sql)){
+			pstmt.executeUpdate();
+			c.commit();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+  
+	@Override
 	public List<Peliculas> selectAllwhereActor(String name) {
 	  List<Peliculas> filmList = new ArrayList<>();
 	  String sql = "SELECT * from peliculas as p " +
--- src/main/java/urjc/isi/entidades/Personas.java
+++ src/main/java/urjc/isi/entidades/Personas.java
@@ -12,86 +12,94 @@ public class Personas {
 	private String fMuerte;
 
 
-  // Constructor
-  public Personas(){
-	  ;
-  }
+	// Constructor
+	public Personas(){}
 
-  public Personas (String id, String fullNombre, String fNacimiento, String fMuerte){
+	public Personas (String id, String fullNombre, String fNacimiento, String fMuerte){
 		this.setId(id);
 		this.setFullNombre(fullNombre);
 		this.setNacimiento(fNacimiento);
 		this.setMuerte(fMuerte);
-  }
+	}
 
-  public Personas(String line){ // Se tokeniza la linea
-	  StringTokenizer tokenizer = new StringTokenizer(line,"\t");
-	  this.setId(tokenizer.nextToken());
-	  this.setFullNombre(tokenizer.nextToken());
-	  this.setNacimiento(tokenizer.nextToken());
-	  this.setMuerte(tokenizer.nextToken());
-  }
+	public Personas(String line){ // Se tokeniza la linea
+		StringTokenizer tokenizer = new StringTokenizer(line,"\t");
+		this.setId(tokenizer.nextToken());
+		this.setFullNombre(tokenizer.nextToken());
+		this.setNacimiento(tokenizer.nextToken());
+		this.setMuerte(tokenizer.nextToken());
+	}
 
-  // Setter Methods
-  public void setId(String id){
-	  if(id == null){
-	      throw new InvalidParameter();
+	// Setter Methods
+	public void setId(String id){
+		if(id == null){
+			throw new InvalidParameter();
 	    }
-    this.id = id;
-  }
-  public void setFullNombre(String fullNombre){
-    if(fullNombre == null){
-      throw new InvalidParameter();
-    }
-    this.fullNombre = fullNombre;
-  }
-  public void setNacimiento(String fNacimiento){
-    this.fNacimiento = fNacimiento;
-  }
-  public void setMuerte(String fMuerte){
-    this.fMuerte = fMuerte;
-  }
+		this.id = id;
+	}
+	
+	public void setFullNombre(String fullNombre){
+		if(fullNombre == null){
+			throw new InvalidParameter();
+		}
+		this.fullNombre = fullNombre;
+	}
+	
+	public void setNacimiento(String fNacimiento){
+		this.fNacimiento = fNacimiento;
+	}
+	
+	public void setMuerte(String fMuerte){
+		this.fMuerte = fMuerte;
+	}
 
-  // Getter Methods
-  public String getId() {
-	  return id;
-  }
-  public String getFullNombre() {
-	  return fullNombre;
-  }
-  public String getNacimiento(){
-      return fNacimiento;
-  }
-  public String getMuerte(){
-      return fMuerte;
-  }
-  // Overrided Methods
+	// Getter Methods
+	public String getId() {
+		return id;
+	}
+	
+	public String getFullNombre() {
+		return fullNombre;
+	}
+	
+	public String getNacimiento(){
+		return fNacimiento;
+	}
+	
+	public String getMuerte(){
+		return fMuerte;
+	}
+	
+	// Overrided Methods
 	@Override
 	public boolean equals(Object other) {
-    if (other == null) return false;
-    if (other == this) return true;
-    if (!(other instanceof Personas)) return false;
-
-    Personas otherP = (Personas)other;
-
-    return Objects.equals(this.id,otherP.id) &&
-              Objects.equals(this.fullNombre,otherP.fullNombre) &&
-              Objects.equals(this.fNacimiento, otherP.fNacimiento) &&
-              Objects.equals(this.fMuerte, otherP.fMuerte);
+	    if (other == null) return false;
+	    if (other == this) return true;
+	    if (!(other instanceof Personas)) return false;
+	
+	    Personas otherP = (Personas)other;
+	
+	    return Objects.equals(this.id,otherP.id) &&
+	              Objects.equals(this.fullNombre,otherP.fullNombre) &&
+	              Objects.equals(this.fNacimiento, otherP.fNacimiento) &&
+	              Objects.equals(this.fMuerte, otherP.fMuerte);
 	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id,fullNombre,fNacimiento,fMuerte);
 	}
+	
     @Override
     public String toString(){
-      return "Id: "+getId()+"\tNombre: " + getFullNombre() +
-      "\tFecha Nacimiento: "+getNacimiento()+
-      "\tFecha Muerte: "+getMuerte();
+    	return "Id: "+getId()+"\tNombre: " + getFullNombre() +
+    			"\tFecha Nacimiento: "+getNacimiento()+
+    			"\tFecha Muerte: "+getMuerte();
     }
+    
 	public String toHTMLString() { //Método necesario para una buena respuesta en el servidor
-  	  return "Id: "+getId()+"&emsp; Nombre: " + getFullNombre() +
-  	  "&emsp; Fecha nacimiento: "+getNacimiento()+
-        "&emsp; Fecha muerte: "+getMuerte();
+		return "Id: "+getId()+"&emsp; Nombre: " + getFullNombre() +
+				"&emsp; Fecha nacimiento: "+getNacimiento()+
+				"&emsp; Fecha muerte: "+getMuerte();
     }
 }
