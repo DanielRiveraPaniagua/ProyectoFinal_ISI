61d75cfb4dbc96cb5609b21466af4106f0a5442c
ISMAEL <isma98.igm@gmail.com>
2019-12-29 18:56:02 +0100

    arreglando rating, como double precision

--- src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
+++ src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
@@ -34,7 +34,7 @@ public class PeliculasDAOImpl extends GenericDAOImpl<Peliculas> implements Pelic
 	@Override
 	public void createTable() throws SQLException{
 		Statement statement = c.createStatement();
-		statement.executeUpdate("create table peliculas (idpelicula text, titulo text, año text, duracion text, calificacion INT,rating text, nvotos INT, PRIMARY KEY (idpelicula))");
+		statement.executeUpdate("create table peliculas (idpelicula text, titulo text, año text, duracion text, calificacion INT,rating double precision, nvotos INT, PRIMARY KEY (idpelicula))");
 		c.commit();	
 	}
   
