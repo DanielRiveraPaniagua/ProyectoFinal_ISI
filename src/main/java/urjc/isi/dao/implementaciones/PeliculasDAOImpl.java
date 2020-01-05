63c19fbba10f078b9d8b8a7c65a708ae93a30895
Ana Gallego <rozalengallego@gmail.com>
2020-01-05 10:42:25 +0100

    peliculasdao

--- src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
+++ src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
@@ -191,8 +191,7 @@ public class PeliculasDAOImpl extends GenericDAOImpl<Peliculas> implements Pelic
 	@Override
 	public List<Peliculas> selectPeliculasForAdultos(){
 		List<Peliculas> filmList = new ArrayList<>();
-		String sql = "SELECT from peliculas as p" +
-		"p.titulo " + "where p.calificacion = 1";
+		String sql = "SELECT * from peliculas as p where p.calificacion = 1";
 		try (PreparedStatement pstmt = c.prepareStatement(sql)) {
 			ResultSet rs = pstmt.executeQuery();
 			c.commit();
