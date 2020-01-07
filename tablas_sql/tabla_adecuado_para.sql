c3d8d33cb211cf8a27743e750f874105a901e676
felipecordente <felipe_1998_@outlook.com>
2020-01-07 12:43:54 +0100

    vaciado de rama

--- tablas_sql/tabla_adecuado_para.sql
+++
@@ -1,9 +0,0 @@
-.open sample.db
-
-CREATE TABLE adecuado_para(
-	id_edades INT,
-	id_pelicula INT,
-	PRIMARY KEY(id_edades, id_peliculas),
-	FOREING KEY(id_edades REFERENCES Calificacion(id_edades),
-	FOREING KEY(id_pelicula) REFERENCES Peliculas(id_pelicula) ON DELETE CASCADE
-);
--- tablas_sql/tabla_guionistas.sql
+++
@@ -1,9 +0,0 @@
-.open sample.db
-
-CREATE TABLE Guionistas(
-	id_guionista INTEGER NOT NULL,
-	nombre_y_apellido TEXT NOT NULL,
-	año_nacimiento TEXT NOT NULL,
-	año_muerte TEXT NOT NULL,
-	PRIMARY KEY(id_guionista)
-);
