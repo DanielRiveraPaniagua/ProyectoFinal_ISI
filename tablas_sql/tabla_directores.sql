3d13d5e89e5de0ee524e2aeb97242a483ec1ed12
felipecordente <felipe_1998_@outlook.com>
2020-01-07 13:08:39 +0100

    borro todo

--- tablas_sql/tabla_directores.sql
+++
@@ -1,9 +0,0 @@
-.open sample.db
-
-CREATE TABLE Directores(
-	id_director INTEGER NOT NULL,
-	nombre_y_apellido TEXT NOT NULL,
-	año_nacimiento TEXT NOT NULL,
-	año_muerte TEXT NOT NULL,
-	PRIMARY KEY(id_director)
-);
