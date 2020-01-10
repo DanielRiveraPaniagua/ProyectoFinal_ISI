15501b17728db7197329ec5495fc3df98ab3bcd8
angeisa97 <ange.isabel@hotmail.com>
2020-01-10 19:20:58 +0100

    arreglando fallo en pagina /rankin 2

--- src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
+++ src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
@@ -174,7 +174,7 @@ public class PeliculasDAOImpl extends GenericDAOImpl<Peliculas> implements Pelic
 	@Override
 	public List<Peliculas> selectBest10(){
 		List<Peliculas> bestList = new ArrayList<>();
-		String sql = "SELECT * from peliculas ORDER BY rating DESC LIMIT 10";
+		String sql = "SELECT titulo, rating, nvotos from peliculas ORDER BY rating DESC LIMIT 10";
 		try (PreparedStatement pstmt = c.prepareStatement(sql)) {
 			ResultSet rs = pstmt.executeQuery();
 			c.commit();
