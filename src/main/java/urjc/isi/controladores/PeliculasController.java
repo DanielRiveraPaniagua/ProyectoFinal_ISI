7c990241513528b7e3f441fded7e89fc5ed790ee
DanielRiveraPaniagua <danirpclase@gmail.com>
2020-01-15 16:11:10 +0100

    controller adaptation 3

--- src/main/java/urjc/isi/controladores/PeliculasController.java
+++ src/main/java/urjc/isi/controladores/PeliculasController.java
@@ -186,7 +186,7 @@ public class PeliculasController {
 		Dictionary<String,Object> output;
 		String result = "";
 
-		result = ps.fullPeliculasInfo(request.queryParams("titulo"));
+		output = ps.fullPeliculasInfo(request.queryParams("titulo"));
 		Peliculas pelicula = (Peliculas)output.get("pelicula");
 		List<Personas> actores = (List<Personas>)output.get("actores");
 		List<Personas> guionistas = (List<Personas>)output.get("guionistas");
