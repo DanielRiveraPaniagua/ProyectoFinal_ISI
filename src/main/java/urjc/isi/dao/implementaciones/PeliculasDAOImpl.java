43498dd67b7904cb9a8afd69a1c3bc321f4ae7a0
oscarescuderoarnanz <escuderoarnanzoscar@gmail.com>
2020-01-16 22:02:25 +0100

    Funciona con = y rango, aun falta < y >

--- src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
+++ src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
@@ -141,9 +141,31 @@ public class PeliculasDAOImpl extends GenericDAOImpl<Peliculas> implements Pelic
 					cond+= "g.fullnombre LIKE "+"'"+conditions.get("guionista")+"'";
 					break;
 				case "duracion":
-					String[] partsmayor = conditions.get("duracion").split(">");
-					double a = Double.parseDouble(partsmayor[1]);
-					cond+= "duracion>="+ "'" + a + "'";
+					//String[] partsmayor = conditions.get("duracion").split(">");
+					//double a = Double.parseDouble(partsmayor[1]);
+					//conditions.get("duracion").charAt(0);
+					//conditions.get("duracion").substring(1, conditions.get("duracion").length());
+					//cond+= "duracion ="+conditions.get("duracion").charAt(0) + " '" + conditions.get("duracion").substring(1, conditions.get("duracion").length()) + "'";
+					//String a = conditions.get("duracion");
+					//String x = a.replaceAll("[<]", "");
+					//double y = Double.parseDouble(x);
+					//cond+= "p.duracion =< " + "'" + y + "'";
+					//if(conditions.get("duracion").indexOf("-") == -1) {
+						//cond+= "p.duracion = "+"'"+conditions.get("duracion")+"'";
+					//} else {
+						//String[] years = conditions.get("duracion").split("-");
+						//cond+= "p.duracion >= " + "'" + years[0] + "'" + " and " + "p.duracion <= "+ "'"+ years[1] + "'" ;
+					//}
+					if(conditions.get("duracion").indexOf("-") == -1) {
+						cond+= "p.duracion = "+"'"+conditions.get("duracion")+"'";
+					}else if(conditions.get("duracion").indexOf("<") == 0) {
+						cond+= "p.duracion <= "+"'"+conditions.get("duracion").split("<")[1]+"'";
+					}else if(conditions.get("duracion").indexOf(">") == 0){
+						cond+= "p.duracion >= "+"'"+conditions.get("duracion").split(">")[1]+"'";
+					} else {
+						String[] duracion = conditions.get("duracion").split("-");
+						cond+= "p.duracion >= " + "'" + duracion[0] + "'" + " and " + "p.duracion <= "+ "'"+ duracion[1] + "'" ;
+					}
 					break;
 				case "adultos":
 					if(conditions.get("adultos").equals("si"))
