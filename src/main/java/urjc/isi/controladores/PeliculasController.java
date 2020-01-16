e00627105f967b754e7135186e498dc9bcb274c6
oscarescuderoarnanz <escuderoarnanzoscar@gmail.com>
2020-01-16 20:17:23 +0100

    Primera prueba funcionando

--- src/main/java/urjc/isi/controladores/PeliculasController.java
+++ src/main/java/urjc/isi/controladores/PeliculasController.java
@@ -97,7 +97,7 @@ public class PeliculasController {
 			    result = result + output.get(i).toHTMLString() +"</br>";
 			}
 		}
-		return filter + "<br/>" + result;
+		return result;
 	}
 	
 	public static String selectAllRanking(Request request, Response response) throws SQLException {
--- src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
+++ src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
@@ -121,7 +121,6 @@ public class PeliculasDAOImpl extends GenericDAOImpl<Peliculas> implements Pelic
 	@Override
 	public List<Peliculas> selectAll(Dictionary<String,String> conditions){
 		List<Peliculas> filmList = new ArrayList<>();
-		String querysql;
 		String sql = "SELECT * from peliculas as p ";
 		String cond = "WHERE ";
 		for(Enumeration<String> k = conditions.keys(); k.hasMoreElements();) {
@@ -142,18 +141,9 @@ public class PeliculasDAOImpl extends GenericDAOImpl<Peliculas> implements Pelic
 					cond+= "g.fullnombre LIKE "+"'"+conditions.get("guionista")+"'";
 					break;
 				case "duracion":
-					cond+= "p.duracion>"+"'"+conditions.get("duracion")+"'";
-					if(conditions.get("duracion").length() == 1) {
-						querysql = conditions.get("duracion");
-						char simbolo = querysql.charAt(0);
-						if(simbolo == '>') {
-							String[] partsmayor = querysql.split(">");
-							String time1 = partsmayor[1];
-							double t1 = Double.parseDouble(time1);
-							cond+= "p.duracion>"+ t1 + " ORDER BY ASC";
-						}
-						
-					}
+					String[] partsmayor = conditions.get("duracion").split(">");
+					double a = Double.parseDouble(partsmayor[1]);
+					cond+= "duracion>="+ "'" + a + "'";
 					break;
 				case "adultos":
 					if(conditions.get("adultos").equals("si"))
@@ -203,7 +193,7 @@ public class PeliculasDAOImpl extends GenericDAOImpl<Peliculas> implements Pelic
 		}
 		return bestList;
 	}
-	
+
 	@Override
 	public List<Peliculas> selectByRanking(Dictionary<String,String> conditions){
 		List<Peliculas> filmList = new ArrayList<>();
