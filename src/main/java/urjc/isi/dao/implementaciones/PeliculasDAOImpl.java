e095f486c1e863ad81d2edb1bf02e124d512b65e
oscarescuderoarnanz <escuderoarnanzoscar@gmail.com>
2020-01-16 22:04:37 +0100

    Pruebo con < > a capon

--- src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
+++ src/main/java/urjc/isi/dao/implementaciones/PeliculasDAOImpl.java
@@ -159,9 +159,11 @@ public class PeliculasDAOImpl extends GenericDAOImpl<Peliculas> implements Pelic
 					if(conditions.get("duracion").indexOf("-") == -1) {
 						cond+= "p.duracion = "+"'"+conditions.get("duracion")+"'";
 					}else if(conditions.get("duracion").indexOf("<") == 0) {
-						cond+= "p.duracion <= "+"'"+conditions.get("duracion").split("<")[1]+"'";
+						//cond+= "p.duracion <= "+"'"+conditions.get("duracion").split("<")[1]+"'";
+						cond+= "p.duracion >= " + "'"+ 189+ "'";
 					}else if(conditions.get("duracion").indexOf(">") == 0){
-						cond+= "p.duracion >= "+"'"+conditions.get("duracion").split(">")[1]+"'";
+						//cond+= "p.duracion >= "+"'"+conditions.get("duracion").split(">")[1]+"'";
+						cond+= "p.duracion <= " + "'"+ 189+ "'";
 					} else {
 						String[] duracion = conditions.get("duracion").split("-");
 						cond+= "p.duracion >= " + "'" + duracion[0] + "'" + " and " + "p.duracion <= "+ "'"+ duracion[1] + "'" ;
