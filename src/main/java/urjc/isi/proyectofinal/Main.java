9bc9f45e7f156342e9f2589ecd5a03156de03387
oscar <47352810+oscarescuderoarnanz@users.noreply.github.com>
2020-01-17 10:27:29 +0100

    Añado mensaje original de welcome

--- src/main/java/urjc/isi/proyectofinal/Main.java
+++ src/main/java/urjc/isi/proyectofinal/Main.java
@@ -22,7 +22,7 @@ public class Main {
 	 * @throws URISyntaxException
 	 */
 	public static String defaultResponse(Request request, Response response) throws ClassNotFoundException, URISyntaxException {
-       String result = new String("prueba 1");
+       String result = new String("Film application is in WIP. THANKS!");
        return result;
     }
 
