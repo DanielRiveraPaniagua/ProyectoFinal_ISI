2da8c06125593b409ab8937085b65184e46968a1
mariaffs <mari271997@gmail.com>
2020-01-17 19:43:44 +0100

    intervalos cambiados y aladido selectByName en Guionistas y Directores Impl

--- src/main/java/urjc/isi/dao/interfaces/PersonasDAO.java
+++ src/main/java/urjc/isi/dao/interfaces/PersonasDAO.java
@@ -1,5 +1,6 @@
 package urjc.isi.dao.interfaces;
 
+import urjc.isi.entidades.Generos;
 import urjc.isi.entidades.Personas;
 
 import java.util.Dictionary;
@@ -16,4 +17,7 @@ public interface PersonasDAO extends GenericDAO<Personas>{
 	 * @return
 	 */
 	public List<Personas> selectAll(Dictionary<String,String> conditions); //probablemente debería estar en generic
+
+	public Personas selectByName(String name);
+	
 }
