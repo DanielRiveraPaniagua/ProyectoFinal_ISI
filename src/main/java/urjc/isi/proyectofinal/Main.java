141d024aab1247095f8627a502c22681aa5a0dc0
Ana Gallego <rozalengallego@gmail.com>
2020-01-19 20:39:36 +0100

    url actores

--- src/main/java/urjc/isi/proyectofinal/Main.java
+++ src/main/java/urjc/isi/proyectofinal/Main.java
@@ -48,7 +48,8 @@ public class Main {
 					"<p><strong><span style=color: #0000ff;>Actores</span></strong></p><ul>"+
 					"<li><a href=/actores/uploadTable?key=1234>Subir tablas</a></li>"+
 					"<li><a href=/actores/selectAll>Mostrar todos los actores</a></li> "+
-					"</ul>" +
+					"<ul><li>/actores/selectAll?[id_act,name,fecha_nac,fecha_muer,guionista,director,titulo]=[Dato a buscar]</li></ul>"
+					"</ul>" + 
 					"<p><span style=color: #0000ff;><strong>Guionistas</strong></span></p></ul>" +
 					"<li><span style=color: #0000ff;><a href=/guionistas/uploadTable?key=1234>Subir tablas</a></span></li>" +
 					"<li><span style=color: #0000ff;><a href=/guionistas/selectAll>Mostrar todos los guionistas </a></li></ul>"+
@@ -63,68 +64,9 @@ public class Main {
 					"</ul>" +
 					"</body></html>";
 
-		result_Victor = "<span style=bgcolor:#FF7247;</span><form action='/pag_principal' method='post' enctype='multipart/form-data'>"
-        		+ "<h1 style=color:#1821EB;>Bienvenido a nuestra web de peliculas</h1>"
-        		+ "<br>" + "<h2 style=color:#5B72EB;>ACTORES</h2>" + "<br>"
-        		+ "<a href=https://peliculasurjc.herokuapp.com/actores/selectAll>Imprimir todos los actores</a>"
-        		+ "<p>Tambien puedes filtrar los actores por:</p>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/actores/selectAll?id_act=x>id</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/actores/selectAll?name=x>nombre</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/actores/selectAll?fecha_nac=x>fecha de nacimiento</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/actores/selectAll?fecha_nac=x-x>intervalo fecha de nacimiento</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/actores/selectAll?fecha_muer=x>fecha de muerte</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/actores/selectAll?fecha_muer=x-x>intervalo fecha de muerte</a>" + "<br>"
-        		+ "<p>Sustituyendo las x por los valores deseados</p>"
-        		+ "<br>" + "<h2 style=color:#5B72EB;>PELICULAS</H2>" + "<br>"
-        		+ "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll>Imprimir todas las peliculas</a>"
-        		+ "<p>Tambien puedes filtrar las peliculas por:</p>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll?titulo=x>titulo</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll?actor=x>actor</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll?director=x>director</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll?guionista=x>guionista</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll?year=x>año</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll?year=x-x>intervalo de años</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll?idioma=x>idioma</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll?adultos=x>adultos(si/no)</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll?duracion=>x>duracion mayor</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll?duracion=<x>duracion menor</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll?duracion=x>duracion</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll?duracion=x-x>intervalo de duracion</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll?rating=x-x>intervalo de puntuacion</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll?rating=>x>puntuacion mayor</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll?rating=<x>puntuacion menor</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/selectAll?rating=x>puntuacion</a>" + "<br>"
-        		+ "<p>Sustiyendo las x por los valores deseados</p>"
-        		+ "<br>" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/ranking>Diez peliculas mejor valoradas</a>"
-        		+ "<p>Tambien puedes filtrar las peliculas por:</p>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/ranking?actor=x>actor</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/ranking?director=x>director</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/ranking?guionista=x>guionista</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/peliculas/ranking?genero=x>genero</a>" + "<br>"
-        		+ "<p>Sustituyendo las x por los valores deseados</p>"
-        		+ "<br>" + "<h2 style=color:#5B72EB;>DIRECTORES</h2>" + "<br>"
-        		+ "<a href=https://peliculasurjc.herokuapp.com/directores/selectAll>Imprimir todos los directores</a>"
-        		+ "<p>Tambien puedes filtrar los directores por:</p>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/directores/selectAll?id_dir=x>id</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/directores/selectAll?name=x>nombre</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/directores/selectAll?fecha_nac=x>fecha de nacimiento</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/directores/selectAll?fecha_nac=x-x>intervalo fecha de nacimiento</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/directores/selectAll?fecha_muer=x>fecha de muerte</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/directores/selectAll?fecha_muer=x-x>intervalo fecha de muerte</a>" + "<br>"
-        		+ "<p>Sustituyendo las x por los valores deseados</p>"
-        		+ "<br>" + "<h2 style=color:#5B72EB;>GUIONISTAS</h2>" + "<br>"
-        		+ "<a href=https://peliculasurjc.herokuapp.com/guionistas/selectAll>Imprimir todos los guionistas</a>"
-        		+ "<p>Tambien puedes filtrar los guionistas por:</p>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/guionistas/selectAll?id_guio=x>id</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/guionistas/selectAll?name=x>nombre</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/guionistas/selectAll?fecha_nac=x>fecha de nacimiento</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/guionistas/selectAll?fecha_nac=x-x>intervalo fecha de nacimiento</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/guionistas/selectAll?fecha_muer=x>fecha de muerte</a>" + "<br>"
-        		+ "-->" + "<a href=https://peliculasurjc.herokuapp.com/guionistas/selectAll?fecha_muer=x-x>intervalo fecha de muerte</a>" + "<br>"
-        		+ "<p>Sustituyendo las x por los valores deseados</p>"
-        		+ "<br>" + "<h2 style=color:#5B72EB;>GENEROS</h2>" + "<br>"
-        		+ "<a href=https://peliculasurjc.herokuapp.com/generos/selectAll>Imprimir todos los generos</a>"
-        		+ "</form>";
+		
+        	
+        		
 
 		return result;
     }
