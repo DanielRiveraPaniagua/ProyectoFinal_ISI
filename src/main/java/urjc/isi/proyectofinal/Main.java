10558d5ed9954010771daf9f3adc0f988dc36bdb
angeisa97 <ange.isabel@hotmail.com>
2020-01-19 21:23:19 +0100

    final /welcome

--- src/main/java/urjc/isi/proyectofinal/Main.java
+++ src/main/java/urjc/isi/proyectofinal/Main.java
@@ -52,7 +52,6 @@ public class Main {
 					"<ul><li> /peliculas/selectAll?rating=>r1 --> Películas con rating mayor o igual al dado </li></ul>"+
 					"<ul><li> /peliculas/selectAll?rating=r1 --> Películas con rating igual al dado </li></ul>"+
 					"<li> /peliculas/selectAll?order=[x] --> Ordena según el criterio </li>"+
-					"</ul>" +
 					"<li><a href=/peliculas/calificacion>Mostrar calificación de la película introducida</a></li> "+
 					"</ul>" +
 					"<p><strong><span style=color: #0000ff;>Actores</span></strong></p><ul>"+
@@ -61,13 +60,13 @@ public class Main {
 					"<ul><li>/actores/selectAll?[id_act,name,fecha_nac,fecha_muer,guionista,director,título]=[Dato a buscar]</li></ul>"+
 					"</ul>" + 
 					"<p><span style=color: #0000ff;><strong>Guionistas</strong></span></p></ul>" +
-					"<li><span style=color: #0000ff;><a href=/guionistas/uploadTable?key=1234>Subir tablas</a></span></li>" +
-					"<li><span style=color: #0000ff;><a href=/guionistas/selectAll>Mostrar todos los guionistas </a></li></ul>"+
+					"<li><a href=/guionistas/uploadTable?key=1234>Subir tablas</a></li>" +
+					"<li><a href=/guionistas/selectAll>Mostrar todos los guionistas </a></li></ul>"+
 					"<ul><li>/guionistas/selectAll?[id_act,name,fecha_nac,fecha_muer,actor,director,título]=[Dato a buscar]</li></ul>"+
 					"</ul>" +
 					"<p><span style=color: #0000ff;><strong>Directores</strong></span></p><ul>"+
-					"<li><span style=color: #0000ff;><a href=/directores/uploadTable?key=1234>Subir tablas</a></span></li>"+
-					"<li><span style=color: #0000ff;><a href=/directores/selectAll>Mostrar todos los directores</a></span></li>"+
+					"<li><a href=/directores/uploadTable?key=1234>Subir tablas</a></li>"+
+					"<li><a href=/directores/selectAll>Mostrar todos los directores</a></li>"+
 					"<ul><li>/directores/selectAll?[id_act,name,fecha_nac,fecha_muer,actor,guionista,título]=[Dato a buscar]</li></ul>"+
 					"</ul>" +
 					"<p><span style=color: #0000ff;><strong>Géneros</strong></span></p></ul>"+
